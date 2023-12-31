020810a81747ca1df97d6b22ff6a756bff966c4b
Hassan Oladipupo <109126045+Hassan-oladipupo@users.noreply.github.com>
2023-12-31 22:45:46 +0100

    techsphere

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once 'C:\Users\hp\TechsphereApi\vendor\autoload.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
